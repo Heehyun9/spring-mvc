b23fb80601813717f89233c538f53cd5b1e637f4
노희현 <81168262+Heehyun9@users.noreply.github.com>
2025-09-15 14:55:14 +0900

    HTTP 객체를 데이터로 처리하는 방법 @PathVariable @RequestParam @ModelAttribute @RequestBody

---
+++ src/main/java/com/sparta/springmvc/request/Star.java
@@ -0,0 +1,14 @@
+package com.sparta.springmvc.request;
+
+public class Star {
+    String name;
+    int age;
+
+    public Star(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+}
+
+
